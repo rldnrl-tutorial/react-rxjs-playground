b83f748ef16bf1a837d4c9dba1b41240254ea8e8
rldnrl <devpark1002@gmail.com>
2022-06-06 13:37:17 +0900

    feat: deck 컴포넌트 구현

---
+++ src/home/components/Deck.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useObservableState } from 'observable-hooks'
+
+import { usePokemon } from '@/libs/store'
+import Card from '@/libs/ui/Card'
+
+export default function Deck() {
+  const { deck$ } = usePokemon()
+  const deck = useObservableState(deck$, [])
+  return (
+    <div>
+      <h2>Deck</h2>
+      <div>
+        {deck.map((p) => (
+          <Card
+            key={p.id}
+            imageUrl={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`}
+            alt={p.name}
+            title={p.name}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
